9590736c0386ea7cf9df24fa776d115b6ce01ee9
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2019-03-19 20:24:43 +0100

    Promjena konfiguracije repozitorija

--- src/main/java/hr/kruno/gradivniblokovi/MainActivity.java
+++ src/main/java/hr/kruno/gradivniblokovi/MainActivity.java
@@ -18,19 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
      }
 
     // Metoda prikazSlike je deklarativno navedena u XML datoteci aktivnosti uz
     // tipku PRIKAŽI SLIKU.
     public void prikazSlike(View v) {
-        ImageView iv = (ImageView) findViewById(R.id.imageView);
-        iv.setImageResource(R.drawable.image);
+        ImageView iv = findViewById(R.id.imageView);
+        iv.setImageResource(R.drawable.s2);
     }
 
     public void prikazWeba(View v) {
-        String url = "http://www.unidu.hr";
-        WebView webview = (WebView) findViewById(R.id.webView);
-        webview.getSettings().setJavaScriptEnabled(true);
+        String url = "https://material.io/design/layout/understanding-layout.html";
+        WebView webview = findViewById(R.id.webView);
+        //webview.getSettings().setJavaScriptEnabled(true);
         webview.loadUrl(url);
 
 
@@ -43,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void posaljiSms(View v){
         String broj = "1234567";
-        String tekstPoruke = "Nova SMS poruka";
+        String tekstPoruke = "Dobar dan, hocemo li na kavu?";
         // implicitna namjera - pokreće se standardna aplikacija za slanje poruka
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + broj));
         intent.putExtra("sms_body", tekstPoruke);
