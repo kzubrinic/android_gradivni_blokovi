97506ac1fbc4173670ea14aad6b3ce9daf5f2e89
Krunoslav Zubrinic <kruno.zubrinic@gmail.com>
2020-03-03 19:41:29 +0100

    Optimizacija importa

--- app/src/main/java/hr/unidu/kz/gradivniblokovi/MainActivity.java
+++ app/src/main/java/hr/unidu/kz/gradivniblokovi/MainActivity.java
@@ -6,10 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
