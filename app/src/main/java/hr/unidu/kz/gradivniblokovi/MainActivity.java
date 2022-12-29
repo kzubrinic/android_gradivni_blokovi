82859bc0a7c56d08248271108cc5ca2ede212186
Kruno <kruno.zubrinic@gmail.com>
2022-12-29 20:05:54 +0100

    Upodate vezani uz view binding

--- app/src/main/java/hr/unidu/kz/gradivniblokovi/MainActivity.java
+++ app/src/main/java/hr/unidu/kz/gradivniblokovi/MainActivity.java
@@ -10,27 +10,48 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import hr.unidu.kz.gradivniblokovi.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    // View binding: Potrebno ako se koristi view binding
+    private ActivityMainBinding bind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // View binding: Referenca na komponente layouta
+        bind = ActivityMainBinding.inflate(getLayoutInflater());
+        // View binding: Referenca na "korijen" komponente
+        View view = bind.getRoot();
+        // View binding: povezivanje s viewom
+        setContentView(view);
+        // Ako se ne koristi view binding - povezivanje s viewom
+        /*
         setContentView(R.layout.activity_main);
-
+         */
     }
 
     // Metoda prikazSlike je deklarativno navedena u XML datoteci aktivnosti uz
     // tipku PRIKAŽI SLIKU.
     public void prikazSlike(View v) {
+        // View binding: Pristup komponentama
+        bind.imageView.setImageResource(R.drawable.s2);
+        // Pristup komponentama BEZ korištenja view bindinga
+        /*
         ImageView iv = findViewById(R.id.imageView);
         iv.setImageResource(R.drawable.s2);
+         */
     }
 
     public void prikazWeba(View v) {
         String url = "https://www.google.com/";
+        // View binding: Pristup komponentama
+        bind.webView.loadUrl(url);
+        // Pristup komponentama BEZ korištenja view bindinga
+        /*
         WebView webview = findViewById(R.id.webView);
         //webview.getSettings().setJavaScriptEnabled(true);
         webview.loadUrl(url);
+         */
 
 
     }
